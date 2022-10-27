0cb6aeb6302f7e76f0f9f98c14ebb04a79be2d0b
drspacemn <dontpanicdao@gmail.com>
2022-10-27 07:17:22 -0600

    update cairo examples

--- camp_2/felts/python/felts.py
+++ camp_2/felts/python/felts.py
@@ -1,11 +1,14 @@
 import sys
 
-# FELTS = an integer in the range 0 < x < FELT_SIZE
+# FELTS(STARKS) = an integer in the range 0 < x < FELT_SIZE
+# FELTS(Cairo) = an integer in the range -FELT_SIZE/2 < x < FELT_SIZE/2
 FELT_SIZE=2**251 + 17 * 2**192 + 1
+SIGNED_FELT_SIZE=int(FELT_SIZE/2)
 UINT_SIZE=2**64
 
 print("Felts:")
 print("\tFELT Int Size: ", FELT_SIZE)
+print("\tFELT Int Size: ", SIGNED_FELT_SIZE)
 print("\tSystem UInt Size: ", UINT_SIZE)
 print()
 
