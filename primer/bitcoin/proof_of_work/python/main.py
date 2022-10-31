bba08a038dfb28c8b4791a9d6fb3a5d5ca2726de
Shebin John <admin@remedcu.com>
2022-10-31 11:22:25 +0000

    Added docstring

--- primer/bitcoin/proof_of_work/python/main.py
+++ primer/bitcoin/proof_of_work/python/main.py
@@ -1,10 +1,49 @@
+"""
+sha256 - Used for creating hash.
+time - Used for getting current timestamp.
+json - For encoding the block content.
+"""
 from hashlib import sha256
 import time
 import json
 
 class Block:
+    """
+    Block
+    -----
+    Block class contains the information of a single Block
+    along with a function to calculate the hash.
+
+    Attributes
+    ----------
+    self.index = The Block Number.
+    self.transactions = The Block Transaction(s).
+    self.timestamp = The Block Timestamp.
+    self.previous_hash = The hash of previous block.
+    self.nonce = The block nonce.
+    self.hash = The block hash.
+
+    Methods
+    -------
+    create_hash(self)
+
+    """
 
     def __init__(self, index, transactions, timestamp, previous_hash = '', nonce = 0):
+        """
+        __init__
+        --------
+        Initialized the block parameters.
+
+        Parameters
+        ----------
+        index - The block index number.
+        transactions - The transactions to be added to this particular block.
+        timestamp - The block timestamp.
+        previous_hash - The hash of the previous block.
+        nonce - The block nonce.
+
+        """
         self.index = index
         self.transactions = transactions
         self.timestamp = timestamp
@@ -12,18 +51,65 @@ class Block:
         self.nonce = nonce
 
     def create_hash(self):
+        """
+        create_hash
+        -----------
+        To create the hash of a block.
+
+        Return
+        ------
+        Returns the SHA256 hash of the block.
+
+        """
         encoded_block = json.dumps(self.__dict__, sort_keys=True).encode()
         return sha256(encoded_block).hexdigest()
 
 class Blockchain:
+    """
+    Blockchain
+    ----------
+    Blockchain class contains the blockchain parameters and corresponding methods for proper
+    functioning of that Blockchain.
+
+
+    Attributes
+    ----------
+    self.difficulty = The difficulty of that blockchain mining process.
+    self.chain = Contains the chain of blocks.
+    self.unconfirmed_transactions = Contains the transactions which are not yet confirmed.
+
+    Methods
+    -------
+    create_genesis_block(self)
+    last_block(self)
+    print_block(self)
+    proof_of_work(self, block)
+    add_block(self, block, proof)
+    is_valid_proof(self, block, block_hash)
+    add_new_transaction(self, transactions)
+    mine(self)
+
+    """
 
     def __init__(self):
+        """
+        __init__
+        --------
+        Initialized the Blockchain Parameters and calls the Genesis Block creation.
+
+        """
         self.difficulty = 5
         self.chain = []
         self.unconfirmed_transactions = []
         self.create_genesis_block()
 
     def create_genesis_block(self):
+        """
+        create_genesis_block
+        --------------------
+        Creates the Genesis Block of the Blockchain.
+
+        """
         genesis_block = Block(0, ['Genesis Block'], time.time(), '')
         genesis_block.hash = genesis_block.create_hash()
         print('Mining the block containing "Genesis Block"')
@@ -36,9 +122,25 @@ class Blockchain:
 
     @property
     def last_block(self):
+        """
+        last_block
+        ----------
+        Returns the last block of the Blockchain.
+
+        Return
+        ------
+        Last block of the Blockchain.
+
+        """
         return self.chain[-1]
 
     def print_block(self):
+        """
+        print_block
+        -----------
+        Prints the entire blockchain one block at a time.
+
+        """
         length = len(self.chain)
         for i in range(length):
             print("Block: "+str(self.chain[i].index))
@@ -48,6 +150,20 @@ class Blockchain:
             print("Hash: "+self.chain[i].hash + '\n')
 
     def proof_of_work(self, block):
+        """
+        proof_of_work
+        -------------
+        Calculates the Proof Of Work of a particular block.
+
+        Parameters
+        ----------
+        block - The Block which contains all the block details.
+
+        Return
+        ------
+        The hash of the block passed as parameter.
+
+        """
         block.nonce = 0
         created_hash = block.create_hash()
         while not created_hash.startswith('0' * self.difficulty):
@@ -58,6 +174,21 @@ class Blockchain:
         return created_hash
 
     def add_block(self, block, proof):
+        """
+        add_block
+        ---------
+        Function to add block to the blockchain. Verification of proofs are done.
+
+        Parameters
+        ----------
+        block - The Block which contains all the block details.
+        proof - The hash details of the block for verification.
+
+        Return
+        ------
+        True if verification passes, False otherwise.
+
+        """
         previous_hash = self.last_block.hash
         if previous_hash != block.previous_hash:
             return False
@@ -68,13 +199,48 @@ class Blockchain:
         return True
 
     def is_valid_proof(self, block, block_hash):
+        """
+        is_valid_proof
+        --------------
+        Checks if a proof is valid or not based on difficulty and hash of the block.
+
+        Parameters
+        ----------
+        block - The Block which contains all the block details.
+        block_hash - The hash details of the block for validation.
+
+        Return
+        ------
+        True if validation passes, False otherwise.
+
+        """
         return block_hash.startswith('0' * self.difficulty) and block_hash == block.create_hash()
 
     def add_new_transaction(self, transactions):
+        """
+        add_new_transaction
+        -------------------
+        Function to add new transactions to the unconfirmed list.
+
+        Parameters
+        ----------
+        transactions - The transactions to be added.
+
+        """
         print('Mining the block containing "' + transactions + '"')
         self.unconfirmed_transactions.append(transactions)
 
     def mine(self):
+        """
+        mine
+        ----
+        Function to mine a new block.
+
+        Return
+        ------
+        The index of the block created.
+
+        """
         if not self.unconfirmed_transactions:
             return False
 
@@ -91,6 +257,12 @@ class Blockchain:
         return new_block.index
 
 def main():
+    """
+    main
+    ----
+    The main entry point of the program.
+
+    """
     bitcoin = Blockchain()
 
     bitcoin.add_new_transaction("Send 1 BTC to Nick Cage")
